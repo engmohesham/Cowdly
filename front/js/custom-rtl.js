aaff9681a8ddb00296a83d9a1163f7eb1ab004b9
Ammar Araby <ammararbie424@gmail.com>
2024-10-20 16:08:40 +0300

    nav changes

--- front/js/custom-rtl.js
+++ front/js/custom-rtl.js
@@ -1,1 +1,34 @@
-$(function(){"use strict";$("#portfolio .c-link").on("click",function(){$(this).children("span").text("عرض المزيد"==$(this).children("span").text()?"عرض أقل":"عرض المزيد"),$("#portfolio .row > div:nth-child(n+7").slideToggle()}),$("#related-works .c-link").on("click",function(){$(this).children("span").text("عرض المزيد"==$(this).children("span").text()?"عرض أقل":"عرض المزيد"),$("#related-works .row > div:nth-child(n+7").slideToggle()})});
+$(function () {
+  "use strict";
+  $("#portfolio .c-link").on("click", function () {
+    $(this)
+      .children("span")
+      .text(
+        "عرض المزيد" == $(this).children("span").text()
+          ? "عرض أقل"
+          : "عرض المزيد"
+      ),
+      $("#portfolio .row > div:nth-child(n+7").slideToggle();
+  }),
+    $("#related-works .c-link").on("click", function () {
+      $(this)
+        .children("span")
+        .text(
+          "عرض المزيد" == $(this).children("span").text()
+            ? "عرض أقل"
+            : "عرض المزيد"
+        ),
+        $("#related-works .row > div:nth-child(n+7").slideToggle();
+    });
+});
+
+window.addEventListener("scroll", function () {
+  const element = document.getElementById("fixed-nav");
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 20) {
+    element.classList.add("fixed-nav");
+  } else {
+    element.classList.remove("fixed-nav");
+  }
+});
